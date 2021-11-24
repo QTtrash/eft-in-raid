6bfab63291405b22eb2f857cf2260168e5258c3c
Suren Mouchsiadis <smouchsiadis@gmail.com>
2021-11-24 19:01:31 +0100

    imutilis, some steps forward

--- src/image_recognition.py
+++ src/image_recognition.py
@@ -1,13 +1,20 @@
 import cv2
 import numpy
-from matplotlib import pyplot as plt
+import imutils
 
 eft_full = cv2.imread("./images/negative/eft_full.png")
+eft_full_grey = cv2.cvtColor(eft_full, cv2.COLOR_BGR2GRAY)
+eft_full_smaller = cv2.imread("./images/negative/eft_full.png")
 eft_menu = cv2.imread("./images/positive/eft_menu.png")
 
-result = cv2.matchTemplate(eft_full, eft_menu, cv2.TM_CCOEFF_NORMED)
-minVal, max_val, minLoc, maxLoc = cv2.minMaxLoc(result)
-print(max_val)
+# for scale in numpy.linspace(0.2, 1.0, 20)[::-1]:
 
+
+result = cv2.matchTemplate(eft_full, eft_menu, cv2.TM_CCOEFF_NORMED)
+minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
+print(maxVal)
+print(maxLoc)
+
+#cv2.imshow("Result", eft_full_grey)
 # cv2.waitKey()
 # cv2.destroyAllWindows()
